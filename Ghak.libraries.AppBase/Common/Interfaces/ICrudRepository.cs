bf5c1382283009cd0b165306fa83313b77ded931
gheith3 <alrawahi.gheith@gmail.com>
2023-02-07 15:33:23 +0400

    add Pagination method to ICRUD interface

--- Ghak.libraries.AppBase/Common/Interfaces/ICrudRepository.cs
+++ Ghak.libraries.AppBase/Common/Interfaces/ICrudRepository.cs
@@ -1,11 +1,12 @@
 ﻿using Ghak.libraries.AppBase.Common.Models;
+using Ghak.libraries.AppBase.Utils;
 
 namespace Ghak.libraries.AppBase.Common.Interfaces;
 
 public interface ICrudRepository<TModel, TDto, TModifyModel> : IPrepareData<TModel>
 {
-    // Task<ApiResponse<PaginationList<T>>> Pagination(PaginationListArgs request,
-    //     CancellationToken cancellationToken);
+    Task<ApiResponse<PaginationList<TDto>>> Pagination(PaginationListArgs request,
+        CancellationToken cancellationToken);
 
     Task<ApiResponse<List<ListItem>>> List(string? searchQuery = null, Dictionary<string, object>? args = null);
     Task<ApiResponse<TDto>> Get(string id);
---
+++ Ghak.libraries.AppBase/Utils/PaginationListArgs.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ghak.libraries.AppBase.Utils;
+
+public class PaginationListArgs
+{
+    [Required] public int Page { get; set; } = 1;
+    [Required] public int ItemsPeerPage { get; set; } = 25;
+    public string? SearchQuery { get; set; } = null;
+    
+    public string? OrderBy { get; set; } = "Id";
+    public string? OrderDirection { get; set; } = "DESC";
+
+    public Dictionary<string, object> Args { get; set; } = new();
+
+    public int GetPagNumber()
+    {
+        return Page > 0 ? Page : 1;
+    }
+    
+    public int GetItemsPeerPage()
+    {
+        return ItemsPeerPage > 0 ? ItemsPeerPage : 20;
+    }
+}
